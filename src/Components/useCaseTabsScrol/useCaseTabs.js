ded7047afcfddbea99d2c9830ecb83569a4787d1
SyedAltafHusain <itzaltafhusain@gmail.com>
2024-01-25 12:24:00 +0530

    useCase Details tabs update

--- src/Components/useCaseTabsScrol/useCaseTabs.js
+++ src/Components/useCaseTabsScrol/useCaseTabs.js
@@ -6,12 +6,18 @@ import AssineTo from "../useCaseAssine/assineTo";
 import WorkViewDetails from "../useCaseWorkViewDetails/workViewDetails";
 import UseCaseComments from "../useCaseWorkViewDetailsComments/UseCaseComments";
 
-
 const UseCaseTabs = () => {
   const [activeTab, setActiveTab] = useState("1");
+  const [popoverVisible, setPopoverVisible] = useState();
   const onChange = (key) => {
     setActiveTab(key);
+    if (key === "2") {
+      setPopoverVisible(true);
+    } else {
+      setPopoverVisible(false);
+    }
   };
+
   return (
     <Tabs
       defaultActiveKey="1"
@@ -23,23 +29,32 @@ const UseCaseTabs = () => {
       <TabPane tab="Overview" key="1">
         <AssineTo />
       </TabPane>
-      <TabPane tab="Workflow View" key="2">
-        <div className="my-8">
-        <WorkViewDetails />
+      <TabPane
+        tab="Workflow View"
+        key="2"
+        className="py-10 relative"
+        onClick={() => handlePopoverVisibilityChange(!setPopoverVisible)}
+      >
+        <div className="my-8 mb-[6rem] px-3 relative" >
+          <WorkViewDetails popoverVisible={popoverVisible} />
         </div>
-        <div>
-          <UseCaseComments/>
+        <div className="border-t-2 border-dashed">
+          <UseCaseComments />
+          <UseCaseComments />
+          <UseCaseComments />
+          <UseCaseComments />
+
         </div>
       </TabPane>
-      <TabPane tab="Asset View" key="3">
-        <div className="my-8">
-        {/* <WorkViewDetails /> */}
-        </div>
+      <TabPane
+        tab="Asset View"
+        key="3"
+        onClick={() => handlePopoverVisibilityChange(!setPopoverVisible)}
+      >
+        <div className="my-8">{/* <WorkViewDetails /> */}</div>
       </TabPane>
       <TabPane tab="Planning" key="4">
-        <div className="my-8">
-        {/* <WorkViewDetails /> */}
-        </div>
+        <div className="my-8">{/* <WorkViewDetails /> */}</div>
       </TabPane>
     </Tabs>
   );
--- src/Components/useCaseWorkViewDetailsComments/UseCaseComments.js
+++ src/Components/useCaseWorkViewDetailsComments/UseCaseComments.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import use from "../../../public/assets/user1.svg"
 import comment from "../../../public/assets/Comment.svg"
+import user2 from "../../../public/assets/user0.svg"
 import Image from 'next/image'
 
 const UseCaseComments = () => {
@@ -15,6 +16,23 @@ const UseCaseComments = () => {
                 <h2>Assign Name :</h2>
                 <Image src={use} className='rounded-full w-10 '/>
                 <p>Angela Moss</p>
+            </div>
+            <div className='flex gap-2 items-center'>
+                <Image src={comment}/>
+                <button className='bg-blue-400 text-white p-2'>Action</button>
+            </div>
+        </div>
+      </div>
+
+      <div className="w-[100%] border shadow-md mb-6 mt-4">
+        <h2 className="text-black-500 text-m font-bold p-2 border bg-neutral-200">
+          Design Specs
+        </h2>
+        <div className='flex justify-between px-4'>
+            <div className='flex items-center gap-3 py-6 '>
+                <h2>Assign Name :</h2>
+                <Image src={user2} className='rounded-full w-10 '/>
+                <p>Jenner</p>
             </div>
             <div className='flex gap-2 items-center'>
                 <Image src={comment}/>
