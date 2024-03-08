f212c0bf4f3f1b09e2e3839e46d4a244d3f98783
Mustafa Mohammed <mustafa.mohammed@synectiks.com>
2024-03-08 18:10:18 +0530

    next js config solved

--- next.config.js
+++ next.config.js
@@ -3,16 +3,7 @@ const nextConfig = {
     output: 'export',
     trailingSlash: true,
     reactStrictMode: true,
-    images: {
-        // Add your remotePatterns configuration
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "**",
-            },
-        ],
-    },
 }
 
 
-export default nextConfig;
+module.exports = nextConfig
