0575771e6f43a9fe62467c0be71ff21148d1b386
Mustafa Mohammed <mustafa.mohammed@synectiks.com>
2024-03-25 18:02:36 +0530

    navbar ui changes

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,3 +1,60 @@
+// 'use client'
+// import React from 'react'
+// import {
+//   CaretUpFilled,
+//   MenuFoldOutlined,
+//   MenuUnfoldOutlined,
+//   UploadOutlined,
+//   UserOutlined,
+//   CaretLeftFilled,
+//   CaretRightFilled,
+//   AccountBookFilled,
+//   BellFilled,
+//   ProjectFilled,
+//   IdcardFilled,
+//   SignalFilled,
+//   SearchOutlined,
+//   VideoCameraOutlined,
+//   RightOutlined,
+//   LeftOutlined,
+//   QuestionCircleOutlined,
+//   BellOutlined,
+// } from '@ant-design/icons';
+// import { Layout, Menu, Button, Input, Badge, Avatar, theme, Card, Row, Col } from 'antd';
+// const { Header } = Layout;
+// const Navbar = () => {
+//   return (
+//     <>
+//       <Header className='flex flex-row items-center justify-between w-full'
+//         style={{ padding: 32, position: 'sticky', top: 0, right: 0, zIndex: 10 }}>
+//         {/* <Button theme="light" className='w-1/3' icon={<SearchOutlined />}>Search</Button> */}
+//         <Row gutter={18} justify="end" align='middle' className='w-full'>
+//           <Col span={6} offset={4}>
+//             <Input
+//               placeholder="Search"
+//               suffix={<SearchOutlined />}
+//               style={{
+//                 borderRadius: '20px',
+//                 width: '550px'
+//               }}
+//             />
+//           </Col>
+//           <Col span={6} offset={8}>
+//             <div className="flex items-center gap-4">
+//               <QuestionCircleOutlined style={{ color: '#888', fontSize: '20px' }} />
+//               <Badge count={1} style={{ fontSize: '12px', width: '20px', height: '20px', lineHeight: '20px' }}>
+//                 <BellOutlined style={{ color: '#888', fontSize: '20px' }} />
+//               </Badge>
+//               <Avatar size={40} icon={<UserOutlined />} />
+//             </div>
+//           </Col>
+//         </Row>
+//       </Header>
+//     </>
+//   )
+// }
+
+// export default Navbar
 'use client'
 import React from 'react'
 import {
@@ -20,37 +77,27 @@ import {
   QuestionCircleOutlined,
   BellOutlined,
 } from '@ant-design/icons';
-import { Layout, Menu, Button, Input, Badge, Avatar, theme, Card, Row, Col } from 'antd';
+import { Layout, Menu, Button, Input, Badge, Avatar, theme, Card } from 'antd';
 const { Header } = Layout;
 const Navbar = () => {
   return (
     <>
-      <Header className='flex flex-row items-center justify-between w-full'
+      <Header className='flex flex-row items-center justify-between w-full '
         style={{ padding: 32, position: 'sticky', top: 0, right: 0, zIndex: 10 }}>
+
+        <div className='w-11'></div>
         {/* <Button theme="light" className='w-1/3' icon={<SearchOutlined />}>Search</Button> */}
-        <Row gutter={18} justify="end" align='middle' className='w-full'>
-          <Col span={6} offset={4}>
-            <Input
-              placeholder="Search"
-              suffix={<SearchOutlined />}
-              style={{
-                borderRadius: '20px',
-                width: '550px'
-              }}
-            />
-          </Col>
-          <Col span={6} offset={8}>
-            <div className="flex items-center gap-4">
-              <QuestionCircleOutlined style={{ color: '#888', fontSize: '20px' }} />
-              <Badge count={1} style={{ fontSize: '12px', width: '20px', height: '20px', lineHeight: '20px' }}>
-                <BellOutlined style={{ color: '#888', fontSize: '20px' }} />
-              </Badge>
-              <Avatar size={40} icon={<UserOutlined />} />
-            </div>
-          </Col>
-        </Row>
-      </Header>
-    </>
+        <Input style={{ width: '550px' }} placeholder="search" suffix={<SearchOutlined />} />
+        <div className="right-menu-wrapper">
+          <div className="flex flex-row items-center justify-between gap-9">
+            <QuestionCircleOutlined size={56} className='w-5' style={{ color: "#fff" }} />
+            <Badge count={1} style={{ fontSize: '12px', width: '20px', height: '20px', lineHeight: '20px' }}>
+              <BellOutlined size={64} style={{ color: "#fff" }} />
+            </Badge>
+            <Avatar size={56} icon={<UserOutlined />} />
+          </div>
+        </div>
+      </Header></>
   )
 }
 
