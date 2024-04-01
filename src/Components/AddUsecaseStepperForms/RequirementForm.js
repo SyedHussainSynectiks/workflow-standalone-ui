ceed437c3bc259acec450ae741b09d5bdf8ea79a
Mustafa Mohammed <mustafa.mohammed@synectiks.com>
2024-04-01 09:35:59 +0530

    removing position

--- src/Components/AddUsecaseStepperForms/RequirementForm.js
+++ src/Components/AddUsecaseStepperForms/RequirementForm.js
@@ -831,8 +831,6 @@ const RequirementForm = (stepperState) => {
                                             // setIsDropdownOpen(false)
                                           }}
                                           style={{
-                                            position: 'relative',
-                                            zIndex: 100,
                                             backgroundColor: '#4299e1',
                                             padding: '0.5rem 0.75rem',
                                             color: '#ffffff',
